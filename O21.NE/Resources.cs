bd51c380cc56ec424df114328d27cce8e674f2ac
Friedrich von Never <friedrich@fornever.me>
2023-03-04 18:17:01 +0100

    (#18) Sprites: fix BMP export

--- O21.NE/Resources.cs
+++ O21.NE/Resources.cs
@@ -7,5 +7,7 @@ public record struct NeResourceType(ushort TypeId, NeResource[] Resources);
 /// <param name="ContentOffsetInAlignments">
 /// Content offset relative to beginning of the file, in terms of the alignment shift count.
 /// </param>
-/// <param name="ContentLength">Content length, in bytes.</param>
+/// <param name="ContentLength">
+/// Content length, documented as size in bytes, but actually in terms of alignment shift count, too.
+/// </param>
 public record struct NeResource(ushort ContentOffsetInAlignments, ushort ContentLength);
--- O21.Resources/Sprites.fs
+++ O21.Resources/Sprites.fs
@@ -1,6 +1,7 @@
 ﻿module O21.Resources.Sprites
 
 open System
+open System.Buffers.Binary
 open System.IO
 
 open O21.NE
@@ -11,14 +12,21 @@ let Load(path: string, outDir: string): unit =
     let resources = neFile.ReadResourceTable() |> Seq.toArray
     let mutable i = 0
     for resource in resources[1].Resources do
-        let data = neFile.ReadResourceContent resource
+        let dib = neFile.ReadResourceContent resource
+        let bitness = BinaryPrimitives.ReadInt16LittleEndian dib[14..16]
+        let mutable paletteColors = BinaryPrimitives.ReadInt32LittleEndian dib[32..36]
+        if paletteColors = 0 then paletteColors <- 1 <<< int bitness
+        let pixelDataOffset =
+            14 + // BMP header
+            40 + // DIB header
+            paletteColors * 4 // palette
         let fileContent =
             [|
                 yield! "BM"B
-                yield! BitConverter.GetBytes (data.Length + 14)
+                yield! BitConverter.GetBytes (dib.Length + 14)
                 yield! BitConverter.GetBytes 0us
                 yield! BitConverter.GetBytes 0us
-                yield! BitConverter.GetBytes 0x76
+                yield! BitConverter.GetBytes pixelDataOffset
                 yield! neFile.ReadResourceContent resource
             |]
         File.WriteAllBytes(outDir + "/" + (string i) + ".bmp", fileContent)
