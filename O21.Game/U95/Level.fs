7e9b2191dcab91e25b26ec656256f64fb0afa256
sotiredofyours <h8everrything@gmail.com>
2023-03-10 19:58:27 +0400

    (#24) Add IDisposable

--- O21.Game/U95/Level.fs
+++ O21.Game/U95/Level.fs
@@ -1,5 +1,6 @@
 ﻿module O21.Game.U95.Reader
 
+open System
 open System.Collections.Generic
 open System.Threading.Tasks
 open Microsoft.Xna.Framework.Graphics
@@ -8,11 +9,16 @@ open O21.Game.U95.Parser
 type Level = {
     LevelMap: List<List<Option<Texture2D>>>
 }
-    with
+    with 
         static member Load(directory:string) (textures:ICollection<Texture2D>) (level:int) (part:int):Task<Level> = task{
-            let parser = new Parser(directory, textures);
+            let parser = Parser(directory, textures);
             let level = parser.LoadLevel level part
             return{
                 LevelMap = level;
             }
         }
+        interface IDisposable with
+            member this.Dispose() =
+                for line in this.LevelMap do
+                    for texture in line do
+                        (texture.Value :> IDisposable).Dispose()
