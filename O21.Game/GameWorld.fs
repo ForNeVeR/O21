cce59fd11369b44d2d9f8e67659688b78edc55d3
Friedrich von Never <friedrich@fornever.me>
2023-04-01 20:33:52 +0200

    (#19) GameWorld: introduce MainMenu scene

---
+++ O21.Game/GameWorld.fs
@@ -0,0 +1,41 @@
+namespace O21.Game
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+open O21.Game.U95
+open O21.Game.U95.Parser
+
+type GameScene =
+    | MainMenu
+    | Playing
+
+type GameWorld = {
+    Scene: GameScene
+    CurrentLevel: Level
+}
+
+module GameWorld =
+    let Update (world: GameWorld) (input: Input) (time: Time): GameWorld =
+        match world.Scene with
+        | MainMenu -> world
+        | Playing -> world
+
+    let Render (world: GameWorld) (batch: SpriteBatch) (gameData: U95Data): unit =
+        match world.Scene with
+        | MainMenu -> ()
+        | Playing ->
+            batch.Draw(gameData.Sprites.Background[1], Rectangle(0, 0, 600, 300), Color.White)
+
+            let map = world.CurrentLevel.LevelMap
+            for i = 0 to map.Length-1 do
+                for j = 0 to map[i].Length-1 do
+                    match map[i][j] with
+                    | Brick b ->
+                        batch.Draw(gameData.Sprites.Bricks[b], Rectangle(12*j, 12*i, 12, 12), Color.White)
+                    | _ ->
+                        ()
+            for i = 0 to gameData.Sprites.Fishes.Length-1 do
+                batch.Draw(gameData.Sprites.Fishes[i].LeftDirection[i], Rectangle(60*i, 60*i,
+                                                                        gameData.Sprites.Fishes[i].Height,
+                                                                        gameData.Sprites.Fishes[i].Width), Color.White)
--- O21.Game/O21Game.fs
+++ O21.Game/O21Game.fs
@@ -4,39 +4,21 @@ open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 
 open O21.Game.U95
-open O21.Game.U95.Parser
 
 module O21Game =
-    type World = {
-        CurrentLevel: Level 
-    }
-
     let init (dataDirectory: string) = fun () -> {
+        Scene = MainMenu
         // TODO[#47]: Async commands
         CurrentLevel = (Level.Load dataDirectory 1 2).Result
     }
 
-    let update (input: Input) (time: Time) (world: World) = 
-        world
+    let update (input: Input) (time: Time) (world: GameWorld) =
+        GameWorld.Update world input time
 
-    let draw (batch: SpriteBatch) (gameData: U95Data) (world: World) =
+    let draw (batch: SpriteBatch) (gameData: U95Data) (world: GameWorld) =
         batch.GraphicsDevice.Clear(Color.White)
+        GameWorld.Render world batch gameData
 
-        batch.Draw(gameData.Sprites.Background[1], Rectangle(0, 0, 600, 300), Color.White)
-
-        let map = world.CurrentLevel.LevelMap
-        for i = 0 to map.Length-1 do
-            for j = 0 to map[i].Length-1 do
-                match map[i][j] with
-                | Brick b ->
-                    batch.Draw(gameData.Sprites.Bricks[b], Rectangle(12*j, 12*i, 12, 12), Color.White)
-                | _ ->
-                    ()
-        for i = 0 to gameData.Sprites.Fishes.Length-1 do
-            batch.Draw(gameData.Sprites.Fishes[i].LeftDirection[i], Rectangle(60*i, 60*i,
-                                                                    gameData.Sprites.Fishes[i].Height,
-                                                                    gameData.Sprites.Fishes[i].Width), Color.White)
-            
     let game (dataDirectory: string) = {
         LoadGameData = fun gd ->
             // TODO[#38]: Preloader, combine with downloader
