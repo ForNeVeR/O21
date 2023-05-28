006e0f3c325468c7cc55d5f31b1cbbc8280a596d
Friedrich von Never <friedrich@fornever.me>
2023-05-28 16:16:16 +0200

    Paths: fix help paths

--- O21.Game/Paths.fs
+++ O21.Game/Paths.fs
@@ -7,7 +7,7 @@ let ProgramRoot = Path.GetDirectoryName(Environment.ProcessPath)
 
 let LocalizationFolder = Path.Combine(ProgramRoot, "Localization")
 let TranslationsFolder = Path.Combine(LocalizationFolder, "Translations")
-let HelpFolder = Path.Combine(ProgramRoot, "Help")
+let HelpFolder = Path.Combine(LocalizationFolder, "Help")
 
 let ContentConfigurationFile = Path.Combine(ProgramRoot, "content.json")
 
