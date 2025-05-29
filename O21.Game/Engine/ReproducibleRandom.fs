8866caad2a7841215ea77988315f2798f0a2be38
Friedrich von Never <friedrich@fornever.me>
2025-05-30 00:20:29 +0200

    (#27) Enemies: fish spawn behavior

--- O21.Game/Engine/ReproducibleRandom.fs
+++ O21.Game/Engine/ReproducibleRandom.fs
@@ -13,3 +13,14 @@ type ReproducibleRandom private (backend: Random) = // TODO: Not really reproduc
     /// <para>This is meant to be persisted together with the game data, to save replays.</para>
     /// </summary>
     static member ChooseRandomSeed(): int = Random.Shared.Next()
+
+    /// <summary>Generates a random number in range from zero to <paramref name="boundary"/>.</summary>
+    /// <param name="boundary">A range boundary that's <b>excluded</b> from the range.</param>
+    member _.NextExcluding(boundary: int): int =
+        backend.Next(boundary)
+
+    member _.NextBool(): bool =
+        backend.Next 100 >= 50
+
+    member _.Chance(probability: float): bool =
+        backend.NextDouble() < probability
